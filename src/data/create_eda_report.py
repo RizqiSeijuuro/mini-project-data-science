7b45312452f416cd871622c01c3c6a97894022a0
Rizqiansyah <rizqiansyah52@gmail.com>
2023-02-27 00:09:44 +0700

    Create EDA Report with pandas profiling

---
+++ src/data/create_eda_report.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+data_alert_bogor = pd.read_csv('data/raw/aggregate_alerts_Kota Bogor.csv')
+data_med_irr_bogor = pd.read_csv('data/raw/aggregate_median_irregularities_Kota Bogor.csv')
+data_med_jam_bogor = pd.read_csv('data/raw/aggregate_median_jams_Kota Bogor.csv')
+
+data_alert_bogor_profile = ProfileReport(data_alert_bogor)
+data_alert_bogor_profile.to_file('reports/alert_bogor_report.html')
+
+data_med_irr_bogor_profile = ProfileReport(data_med_irr_bogor)
+data_med_irr_bogor_profile.to_file('reports/med_irr_bogor_report.html')
+
+data_med_jam_bogor_profile = ProfileReport(data_med_jam_bogor)
+data_med_jam_bogor_profile.to_file('reports/med_jam_bogor_report.html')
