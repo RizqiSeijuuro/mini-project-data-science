f5f75c9d97cc6be2545e5db511fa8109c2538353
Rizqiansyah <rizqiansyah52@gmail.com>
2023-03-17 22:24:56 +0700

    Create early EDA

--- src/data/create_eda_report.py
+++ src/data/create_eda_report.py
@@ -1,9 +1,10 @@
 import pandas as pd
-from pandas_profiling import ProfileReport
+import polars as pl
+from ydata_profiling import ProfileReport
 
-data_alert_bogor = pd.read_csv('data/raw/aggregate_alerts_Kota Bogor.csv')
-data_med_irr_bogor = pd.read_csv('data/raw/aggregate_median_irregularities_Kota Bogor.csv')
-data_med_jam_bogor = pd.read_csv('data/raw/aggregate_median_jams_Kota Bogor.csv')
+data_alert_bogor = pl.read_csv('data/raw/aggregate_alerts_Kota Bogor.csv').to_pandas()
+data_med_irr_bogor = pl.read_csv('data/raw/aggregate_median_irregularities_Kota Bogor.csv').to_pandas()
+data_med_jam_bogor = pl.read_csv('data/raw/aggregate_median_jams_Kota Bogor.csv').to_pandas()
 
 data_alert_bogor_profile = ProfileReport(data_alert_bogor)
 data_alert_bogor_profile.to_file('reports/alert_bogor_report.html')
